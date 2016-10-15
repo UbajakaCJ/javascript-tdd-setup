761a3e0d8618e9d7c0fc02b1df1a189dee21257a
UbajakaCJ <ubajaka.chijioke@gmail.com>
2016-10-15 17:03:15 +0100

    created the test cases

--- tests.js
+++ tests.js
@@ -3,22 +3,22 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var lib = require('./lib/sum_of_primes.js');
 
 describe("Test that constants are computed properly", function() {
   it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
+      lib.sum_of_primes(5) ==  10)
     );
   });
   it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
+      lib.sum_of_primes(10) == 17)
     );
   });
   it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
+      lib.sumof_primes(17) == 23)
     );
   });
 });
