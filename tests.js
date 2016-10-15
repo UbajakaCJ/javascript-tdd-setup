e41f568c195494cd5499176127c7ab1858c8d080
UbajakaCJ <ubajaka.chijioke@gmail.com>
2016-10-15 17:24:58 +0100

    modified the test case for the test of null value

--- tests.js
+++ tests.js
@@ -6,19 +6,32 @@ var assert = chai.assert;
 var lib = require('./lib/sum_of_primes.js');
 
 describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
+  it("should give 10 for prime numbers between 1 to 5", function() {
     assert(
       lib.sum_of_primes(5) ==  10)
     );
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
+  it("should give 17 for prime numbers between 1 to 10", function() {
     assert(
       lib.sum_of_primes(10) == 17)
     );
   });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
+  it("should give 58 for prime numbers between 1 to 17", function() {
     assert(
-      lib.sumof_primes(17) == 23)
+      lib.sum_of_primes(17) == 58)
     );
   });
+
+  it("should return an empty list if the input parameter is zero", function() {
+    assert(
+      lib.sum_of_primes(0) == [])
+    );
+  });
+
+  it("should return an empty list if the input parameter is a string", function() {
+    assert(
+      lib.sum_of_primes('string') == [])
+    );
+  });
+
 });
