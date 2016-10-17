39811bc9f67f0eef88085b0abe4adbb8cc269cfc
UbajakaCJ <ubajaka.chijioke@gmail.com>
2016-10-17 04:24:37 +0100

    Modified the algorithm for tests.js

--- tests.js
+++ tests.js
@@ -3,46 +3,46 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/sum_of_primes.js');
+var lib = require('./lib/sumOfPrimes.js');
 
 describe("Test that constants are computed properly", function() {
   it("should give 10 for prime numbers between 1 to 5", function() {
     assert(
-      lib.sum_of_primes(5) ==  10)
+      lib.sumOfPrimes(5) ==  10
     );
   });
   it("should give 17 for prime numbers between 1 to 10", function() {
     assert(
-      lib.sum_of_primes(10) == 17)
+      lib.sumOfPrimes(10) == 17
     );
   });
   it("should give 58 for prime numbers between 1 to 17", function() {
     assert(
-      lib.sum_of_primes(17) == 58)
+      lib.sumOfPrimes(17) == 58
     );
   });
 
   it("should give 77 for prime numbers between 1 to 19", function() {
     assert(
-      lib.sum_of_primes(19) == 77)
+      lib.sumOfPrimes(19) == 77
     );
   });
 
-  it("should return an empty list if the input parameter is zero", function() {
+  it("should return false if the input parameter is zero", function() {
     assert(
-      lib.sum_of_primes(0) == [])
+      lib.sumOfPrimes(0) == false
     );
   });
 
-  it("should return an empty list if the input parameter is a string", function() {
+  it("should return false if the input parameter is a string", function() {
     assert(
-      lib.sum_of_primes('string') == [])
+      lib.sumOfPrimes('string') == false
     );
   });
 
-  it("should return an empty list if the input parameter is a dictionary", function() {
+  it("should return false if the input parameter is a dictionary", function() {
     assert(
-      lib.sum_of_primes({title: "name"}) == [])
+      lib.sumOfPrimes({title: "name"}) == false
     );
   });
 
